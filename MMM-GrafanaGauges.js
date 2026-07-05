305cef3a874dddba1590b7286698e884e9f837d6
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-07-05 11:04:58 +0200

    refactor: apply linting rules

--- MMM-GrafanaGauges.js
+++ MMM-GrafanaGauges.js
@@ -1,92 +1,93 @@
-Module.register("MMM-GrafanaGauges", {
-    // Default module config.
-    defaults: {
-        height: "100",
-        width: "100",
-        refreshInterval: 900,
-        animationSpeed: 1000,
-        https: false,
-        version: 0,
-        hideLogo: true,
-        spacing: "0",
-        align: "left"
-    },
+Module.register('MMM-GrafanaGauges', {
+	// Default module config.
+	defaults: {
+		height: '100',
+		width: '100',
+		refreshInterval: 900,
+		animationSpeed: 1000,
+		https: false,
+		version: 0,
+		hideLogo: true,
+		spacing: '0',
+		align: 'left',
+	},
 
-    // Define start sequence.
-    start: function() {
-        Log.info("Starting module: " + this.name + "!!!!");
-        this.scheduleUpdate(this.config.refreshInterval);
-    },
+	// Define start sequence.
+	start() {
+		Log.info('Starting module: ' + this.name + '!!!!');
+		this.scheduleUpdate(this.config.refreshInterval);
+	},
 
 	// Define required CSS files.
 	getStyles() {
-		return ['MMM-GrafanaGauges.css']
+		return ['MMM-GrafanaGauges.css'];
 	},
 
-    // Override dom generator.
-    getDom: function() {
-        var wrapper = document.createElement("div");
-        wrapper.className = "mmm-grafana-gauges";
-        if (!this.config.host) {
-            Log.error("MMM-GrafanaGauges: config.host is required");
-            return wrapper;
-        }
+	// Override dom generator.
+	getDom() {
+		const wrapper = document.createElement('div');
+		wrapper.className = 'mmm-grafana-gauges';
+		if (!this.config.host) {
+			Log.error('MMM-GrafanaGauges: config.host is required');
+			return wrapper;
+		}
 
-        var protocol = this.config.https === true ? "https://" : "http://";
-        var version = parseInt(this.config.version, 10) || 0;
-        var baseUrl;
+		const protocol = this.config.https === true ? 'https://' : 'http://';
+		const version = Number(this.config.version) || 0;
+		let baseUrl;
 
-        if (version >= 6) {
-            if (!this.config.id) {
-                Log.error("MMM-GrafanaGauges: config.id is required for Grafana 6+");
-                return wrapper;
-            }
-            baseUrl = protocol + this.config.host + ":" + this.config.port + "/d-solo/" + this.config.id + "/" + this.config.dashboardname + "?orgId=" + this.config.orgId + "&fullscreen&kiosk";
-        } else {
-            baseUrl = protocol + this.config.host + ":" + this.config.port + "/dashboard-solo/db/" + this.config.dashboardname + "?orgId=" + this.config.orgId;
-        }
+		if (version >= 6) {
+			if (!this.config.id) {
+				Log.error('MMM-GrafanaGauges: config.id is required for Grafana 6+');
+				return wrapper;
+			}
 
-        var hideLogo = this.config.hideLogo ? "&hideLogo=true" : "";
-        var img = '';
-        if (Array.isArray(this.config.showIDs) && this.config.showIDs.length > 0) {
-            for (var i = 0; i < this.config.showIDs.length; i++) {
-                img += '<iframe src="' + baseUrl + '&panelId=' + this.config.showIDs[i] + hideLogo + '" width="' + this.config.width + '" height="' + this.config.height + '" frameborder="0" scrolling="no"></iframe>';
-            }
-        } else {
-            Log.warn("MMM-GrafanaGauges: config.showIDs is empty or missing");
-        }
+			baseUrl = protocol + this.config.host + ':' + this.config.port
+				+ '/d-solo/' + this.config.id + '/' + this.config.dashboardname
+				+ '?orgId=' + this.config.orgId + '&fullscreen&kiosk';
+		} else {
+			baseUrl = protocol + this.config.host + ':' + this.config.port + '/dashboard-solo/db/' + this.config.dashboardname + '?orgId=' + this.config.orgId;
+		}
 
-        var align = this.config.align || "left";
-        if (align !== "left" && align !== "center" && align !== "right") {
-            Log.warn("MMM-GrafanaGauges: invalid config.align value '" + align + "'. Allowed values: left, center, right. Falling back to left.");
-            align = "left";
-        }
+		const hideLogo = this.config.hideLogo ? '&hideLogo=true' : '';
+		let img = '';
+		if (Array.isArray(this.config.showIDs) && this.config.showIDs.length > 0) {
+			for (let i = 0; i < this.config.showIDs.length; i++) {
+				img += '<iframe src="' + baseUrl + '&panelId=' + this.config.showIDs[i]
+					+ hideLogo + '" width="' + this.config.width + '" height="' + this.config.height
+					+ '" frameborder="0" scrolling="no"></iframe>';
+			}
+		} else {
+			Log.warn('MMM-GrafanaGauges: config.showIDs is empty or missing');
+		}
 
-        wrapper.classList.add("mmm-grafana-gauges--align-" + align);
-        wrapper.style.setProperty("--mmm-grafana-gauges-gap", this.config.spacing || "0");
-        wrapper.innerHTML = img;
-        wrapper.setAttribute("timestamp", new Date().getTime());
-        return wrapper;
-    },
+		const alignInput = this.config.align || 'left';
+		const align = ['left', 'center', 'right'].includes(alignInput) ? alignInput : 'left';
+		if (alignInput !== align) {
+			Log.warn('MMM-GrafanaGauges: invalid config.align value \'' + alignInput + '\'. Allowed values: left, center, right. Falling back to left.');
+		}
 
-    scheduleUpdate: function(delay) {
-        var nextLoad = this.config.refreshInterval;
-        if (typeof delay !== "undefined" && delay >= 0) {
-            nextLoad = delay * 1000; // Convert seconds to millis
-        }
-        var self = this;
-        setTimeout(function() {
-            self.updateFrame();
-        }, nextLoad);
-    },
+		wrapper.classList.add('mmm-grafana-gauges--align-' + align);
+		wrapper.style.setProperty('--mmm-grafana-gauges-gap', this.config.spacing || '0');
+		wrapper.innerHTML = img;
+		wrapper.setAttribute('timestamp', Date.now());
+		return wrapper;
+	},
 
-    updateFrame: function() {
-        if (!this.config.host) {
-            Log.error("Tried to refresh, iFrameReload URL not set!");
-            return;
-        }
+	scheduleUpdate(delay) {
+		const nextLoad = delay !== undefined && delay >= 0 ? delay * 1000 : this.config.refreshInterval;
+		setTimeout(() => {
+			this.updateFrame();
+		}, nextLoad);
+	},
 
-        this.updateDom(this.config.animationSpeed);
-        this.scheduleUpdate(this.config.refreshInterval);
-    }
+	updateFrame() {
+		if (!this.config.host) {
+			Log.error('Tried to refresh, iFrameReload URL not set!');
+			return;
+		}
+
+		this.updateDom(this.config.animationSpeed);
+		this.scheduleUpdate(this.config.refreshInterval);
+	},
 });
--- demo.config.js
+++ demo.config.js
@@ -1,37 +1,37 @@
-let config = {
-  address: '0.0.0.0',
-  ipWhitelist: [],
-  logLevel: ['INFO', 'LOG', 'WARN', 'ERROR', 'DEBUG'],
-  modules: [
-    {
-      module: 'clock',
-      position: 'middle_center',
-    },
-    {
-      module: 'MMM-GrafanaGauges',
-      position: 'top_right',
-      header: 'Grafana test',
-      config: {
-        version: 6,
-        id: 'as8fA8na',
-        host: 'localhost',
-        port: 3000,
-        https: false,
-        dashboardname: 'flowers',
-        orgId: 1,
-        showIDs: [8, 9, 10, 12, 13],
-        width: '220',
-        height: '220',
-        refreshInterval: 60,
-        hideLogo: true,
-        spacing: '10px',
-        align: 'center',
-      },
-    },
-  ],
-}
+const config = {
+	address: '0.0.0.0',
+	ipWhitelist: [],
+	logLevel: ['INFO', 'LOG', 'WARN', 'ERROR', 'DEBUG'],
+	modules: [
+		{
+			module: 'clock',
+			position: 'middle_center',
+		},
+		{
+			module: 'MMM-GrafanaGauges',
+			position: 'top_right',
+			header: 'Grafana test',
+			config: {
+				version: 6,
+				id: 'as8fA8na',
+				host: 'localhost',
+				port: 3000,
+				https: false,
+				dashboardname: 'flowers',
+				orgId: 1,
+				showIDs: [8, 9, 10, 12, 13],
+				width: '220',
+				height: '220',
+				refreshInterval: 60,
+				hideLogo: true,
+				spacing: '10px',
+				align: 'center',
+			},
+		},
+	],
+};
 
-/** ************* DO NOT EDIT THE LINE BELOW ***************/
+/** ************* DO NOT EDIT THE LINE BELOW */
 if (typeof module !== 'undefined') {
-  module.exports = config
+	module.exports = config;
 }
